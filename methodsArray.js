0557d9e500c8050d61d2d163e80ff0c4ac4a34ea
Giovanny Vargas <yovaz05@gmail.com>
2023-08-23 19:26:23 -0500

    clase 23-08-2023

--- methodsArray.js
+++ methodsArray.js
@@ -47,3 +47,30 @@ let FPC = [
   { nombre: "OnceCaldas", ciudad: "Manizales", ligas: 9, libertadores: 1 },
   { nombre: "Pasto", ciudad: "Pasto", ligas: 3, libertadores: 0 },
 ];
+
+//Every (Todos cumplen - boolean)
+let todosTieneLigas = FPC.every((eq) => eq.ligas > 0);
+console.log(todosTieneLigas);
+
+//Some (Alguno cumple - boolean)
+let algunoTieneLibertadores = FPC.some((eq) => eq.libertadores > 0);
+console.log(algunoTieneLibertadores);
+
+//Filter
+let gananLibertadores = FPC.filter((eq) => eq.libertadores > 0).map(
+  (eq) => eq.nombre
+);
+console.log(gananLibertadores);
+
+let gananLibertadoresCiudad = FPC.filter(
+  (eq) => eq.libertadores > 0 && eq.ciudad === "Medellin"
+).map((eq) => eq.nombre);
+console.log(gananLibertadoresCiudad);
+
+//Reduce
+
+let totalLibertadores = FPC.filter((eq) => eq.libertadores > 0).reduce(
+  (contador, eq) => (contador += eq.libertadores),
+  0
+);
+console.log(totalLibertadores);
